13e41cdf05888334a32b6985d4cf7acd88abd6ae
SebC750 <75767177+SebC750@users.noreply.github.com>
2021-03-16 01:12:18 -0400

    Update my_javaScript.js

--- my_javaScript.js
+++ my_javaScript.js
@@ -1,17 +1,35 @@
-
-
+document.addEventListener('DOMContentLoaded', function()
+        {
+        document.querySelector('#form').onsubmit = function()
+        {
+        const li = document.createElement('li');
+        li.innerHTML = document.querySelector("#task").value;
+        document.querySelector("#taskN").append(li);
+        document.querySelector("#task").value = '';
+        const li2 = document.createElement('li2');
+        li2.innerHTML = document.querySelector("#desc").value;
+        document.querySelector("#taskD").append(li2);
+        document.querySelector("#desc").value = '';
+        const li3 = document.createElement('li3');
+        li3.innerHTML = document.querySelector("#importance").value;
+        document.querySelector("#taskS").append(li3);
+        document.querySelector("#importance").value = '';
+        return false;
+    }
+  });
 var tasks = [];
   function setArray(){
-  boxName = document.getElementById('#taskName').value;
-  boxDesc = document.getElementById('#taskDesc').value;
-  boxStatus = document.getElementById('#taskP').value;
+  boxName = document.getElementById('#task').value;
+  boxDesc = document.getElementById('#desc').value;
+  boxStatus = document.getElementById('#status').value;
   tasks.push(boxName,boxDesc,boxStatus);
   var p = "";
    for(var i = 0; i < tasks.length;i++)
    {
          p = p+tasks[i];
    }
-   document.getElementById(p);
+   document.getElementById(p).innerHTML;
+
 }
 function print()
 {
